c6a5fb5a3da1b75ea3df094569ed5b47e27da304
Owen Mariani <mariani.owen@gmail.com>
2024-03-10 14:16:06 -0700

    login restyled

--- financial-tracker-app/src/pages/Login.jsx
+++ financial-tracker-app/src/pages/Login.jsx
@@ -1,73 +1,86 @@
 import React from "react";
 import { LockOutlined, UserOutlined } from "@ant-design/icons";
-import { Button, Checkbox, Form, Input } from "antd";
+import { Button, Checkbox, Form, Input, theme } from "antd";
 import logo from "../assets/logo.png";
+import { Content } from "antd/es/layout/layout";
 
 const Login = () => {
   const onFinish = (values) => {
     console.log("Received values of form: ", values);
   };
+
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
   return (
     <>
-      <img className="logo" src={logo} alt="logo" />
-      <Form
-        name="normal_login"
-        className="login-form"
-        initialValues={{
-          remember: true,
+      <Content
+        style={{
+          background: colorBgContainer,
+          minHeight: 280,
+          padding: "0 48px",
+          borderRadius: borderRadiusLG,
         }}
-        onFinish={onFinish}
       >
-        <Form.Item
-          name="username"
-          rules={[
-            {
-              required: true,
-              message: "Please input your Username!",
-            },
-          ]}
+        <img className="logo" src={logo} alt="logo" />
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{
+            remember: true,
+          }}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            gap: "20px",
+          }}
+          onFinish={onFinish}
         >
-          <Input
-            prefix={<UserOutlined className="site-form-item-icon" />}
-            placeholder="Username"
-          />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: "Please input your Password!",
-            },
-          ]}
-        >
-          <Input
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Item>
-        <Form.Item>
-          <Form.Item name="remember" valuePropName="checked" noStyle>
-            <Checkbox>Remember me</Checkbox>
-          </Form.Item>
-
-          <a className="login-form-forgot" href="">
-            Forgot password
-          </a>
-        </Form.Item>
-
-        <Form.Item>
-          <Button
-            type="primary"
-            htmlType="submit"
-            className="login-form-button"
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "Please input your Username!",
+              },
+            ]}
           >
-            Log in
-          </Button>
-          Or <a href="/register">register now!</a>
-        </Form.Item>
-      </Form>
+            <Input
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="Username"
+              className="form-input"
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your Password!",
+              },
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+              className="form-input"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+            >
+              Log in
+            </Button>
+            Or <a href="/register">register now!</a>
+          </Form.Item>
+        </Form>
+      </Content>
     </>
   );
 };
