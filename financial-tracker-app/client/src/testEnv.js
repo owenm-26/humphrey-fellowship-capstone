d11ee0523583993e530a9ba465aa3792265f930c
Owen Mariani <mariani.owen@gmail.com>
2024-04-07 13:31:42 -0400

    added testEnv.js and reorganized files into client server

---
+++ financial-tracker-app/client/src/testEnv.js
@@ -0,0 +1,5 @@
+console.log("uri:", import.meta.env.VITE_URI);
+console.log("PORT:", import.meta.env.VITE_PORT);
+console.log("DB:", import.meta.env.VITE_DB);
+console.log("Collection:", import.meta.env.VITE_COLLECTION);
+console.log("JWT_Secret:", import.meta.env.VITE_JWT_SECRET);
