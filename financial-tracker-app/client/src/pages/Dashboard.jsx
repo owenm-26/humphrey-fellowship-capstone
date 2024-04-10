3c1c1cfbeb6b67727d512d7a78d5a4c6b27c304d
Owen Mariani <mariani.owen@gmail.com>
2024-04-10 16:39:00 -0400

    Add toggle created

--- financial-tracker-app/client/src/pages/Dashboard.jsx
+++ financial-tracker-app/client/src/pages/Dashboard.jsx
@@ -21,6 +21,8 @@ const Dashboard = ({ handleLogout }) => {
   const [quantity, setQuantity] = useState(0);
   const [cost, setCost] = useState(0);
 
+  const [isLoggingInventory, setIsLoggingInventory] = useState(false);
+
   const logOut = () => {
     handleLogout();
   };
@@ -206,90 +208,101 @@ const Dashboard = ({ handleLogout }) => {
                 {" "}
                 Inventory
               </Typography.Title>
-              <Form
-                style={{
-                  minHeight: 280,
-                  borderRadius: 0,
-                  width: "100%",
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "center",
-                  padding: "20px",
-                }}
-                initialValues={{
-                  remember: true,
-                }}
-                onFinish={() => {
-                  if (itemName.length < 1) {
-                    alert("Please give the item a name");
-                    return;
-                  }
-                  if (quantity < 1) {
-                    alert("Please give a positive quantity");
-                    return;
-                  }
-                  addInventoryItem(businessId, {
-                    itemName,
-                    quantity,
-                    cost,
-                  });
-                  setItemName("");
-                  setQuantity(0);
-                  setCost(0);
-                }}
+              <Button
+                onClick={() => setIsLoggingInventory(!isLoggingInventory)}
               >
-                <Form.Item label="Item Name">
-                  <Input
-                    type="text"
-                    placeholder="Item Name"
-                    allowClear
-                    value={itemName}
-                    onChange={(e) => setItemName(e.target.value)}
-                    rules={[
-                      {
-                        required: true,
-                        message: "Please input an item name",
-                      },
-                    ]}
-                  />{" "}
-                </Form.Item>
-                <Form.Item label="Quantity">
-                  <Input
-                    type="number"
-                    placeholder="Quantity"
-                    allowClear
-                    value={quantity}
-                    onChange={(e) => setQuantity(parseInt(e.target.value, 10))}
-                    rules={[
-                      {
-                        required: true,
-                        message: "Please input a quantity",
-                      },
-                    ]}
-                  />
-                </Form.Item>
-                <Form.Item label="Total Cost">
-                  <Input
-                    type="number"
-                    placeholder="Total Cost"
-                    allowClear
-                    value={cost}
-                    onChange={(e) => setCost(parseInt(e.target.value, 10))}
-                    rules={[
-                      {
-                        required: true,
-                        message: "Please input a cost",
-                      },
-                    ]}
-                  />
-                </Form.Item>
+                {isLoggingInventory ? "Close" : "Add"}
+              </Button>
+              {isLoggingInventory ? (
+                <Form
+                  style={{
+                    minHeight: 280,
+                    borderRadius: 0,
+                    width: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    padding: "20px",
+                  }}
+                  initialValues={{
+                    remember: true,
+                  }}
+                  onFinish={() => {
+                    if (itemName.length < 1) {
+                      alert("Please give the item a name");
+                      return;
+                    }
+                    if (quantity < 1) {
+                      alert("Please give a positive quantity");
+                      return;
+                    }
+                    addInventoryItem(businessId, {
+                      itemName,
+                      quantity,
+                      cost,
+                    });
+                    setItemName("");
+                    setQuantity(0);
+                    setCost(0);
+                  }}
+                >
+                  <Form.Item label="Item Name">
+                    <Input
+                      type="text"
+                      placeholder="Item Name"
+                      allowClear
+                      value={itemName}
+                      onChange={(e) => setItemName(e.target.value)}
+                      rules={[
+                        {
+                          required: true,
+                          message: "Please input an item name",
+                        },
+                      ]}
+                    />{" "}
+                  </Form.Item>
+                  <Form.Item label="Quantity">
+                    <Input
+                      type="number"
+                      placeholder="Quantity"
+                      allowClear
+                      value={quantity}
+                      onChange={(e) =>
+                        setQuantity(parseInt(e.target.value, 10))
+                      }
+                      rules={[
+                        {
+                          required: true,
+                          message: "Please input a quantity",
+                        },
+                      ]}
+                    />
+                  </Form.Item>
+                  <Form.Item label="Total Cost">
+                    <Input
+                      type="number"
+                      placeholder="Total Cost"
+                      allowClear
+                      value={cost}
+                      onChange={(e) => setCost(parseInt(e.target.value, 10))}
+                      rules={[
+                        {
+                          required: true,
+                          message: "Please input a cost",
+                        },
+                      ]}
+                    />
+                  </Form.Item>
 
-                <Form.Item>
-                  <Button type="primary" htmlType="submit">
-                    Record Inventory
-                  </Button>
-                </Form.Item>
-              </Form>
+                  <Form.Item>
+                    <Button type="primary" htmlType="submit">
+                      Record Inventory
+                    </Button>
+                  </Form.Item>
+                </Form>
+              ) : (
+                ""
+              )}
             </Col>
             <Col
               style={{
